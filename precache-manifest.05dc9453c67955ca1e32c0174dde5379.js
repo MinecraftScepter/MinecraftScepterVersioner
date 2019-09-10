619926fb6fe64de0a4c5f18c82d72dba6a779ee5
Dowen <tokenyete@gmail.com>
2019-09-10 17:02:43 +0800

    Updates

---
+++ precache-manifest.05dc9453c67955ca1e32c0174dde5379.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35fa875c1867081a058c0d044850ea54",
+    "url": "/MinecraftScepterVersioner/index.html"
+  },
+  {
+    "revision": "46ec25d99129edadf3b8",
+    "url": "/MinecraftScepterVersioner/static/css/main.686b5f8d.chunk.css"
+  },
+  {
+    "revision": "076b257c7f580e89b6ee",
+    "url": "/MinecraftScepterVersioner/static/js/2.01cd2256.chunk.js"
+  },
+  {
+    "revision": "46ec25d99129edadf3b8",
+    "url": "/MinecraftScepterVersioner/static/js/main.bd951d94.chunk.js"
+  },
+  {
+    "revision": "e0419f03184226ede7d6",
+    "url": "/MinecraftScepterVersioner/static/js/runtime~main.f72fa549.js"
+  }
+]);
